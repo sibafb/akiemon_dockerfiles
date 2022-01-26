efeb141ac93d49e80eb18e1d8c18fda2ddcd0b6e
AkimotoHaruma <siba16fb@gmail.com>
2022-01-27 00:23:25 +0900

    --netまでのコメント記載

---
+++ dockerfiles/ros-melodic/run-docker-container.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+countainar_name="ros-melodic-multi-turtle"
+
+# get parameter from system
+user=`id -un`
+
+# start sharing xhost
+xhost +local:user
+
+# run docker
+# ref https://docs.docker.jp/engine/reference/index.html
+
+# run command note
+#--rm Clen Up https://docs.docker.jp/engine/reference/run.html#rm
+#--net https://docs.docker.jp/engine/reference/run.html#network-settings
+docker run --net=host\
+  --ipc=host \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -v $HOME/work:$HOME/work \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -it --name ${countainar_name} ${user}/ros-bionic-melodic
