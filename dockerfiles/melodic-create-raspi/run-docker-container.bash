63e01596594e776e33d147ff0beeba4388d40a19
AkimotoHaruma <siba16fb@gmail.com>
2022-04-04 15:56:42 +0900

    fix

--- dockerfiles/melodic-create-raspi/run-docker-container.bash
+++ dockerfiles/melodic-create-raspi/run-docker-container.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-countainar_name="ros-melodic-create"
+countainar_name="melodic-create"
 
 # get parameter from system
 user=`id -un`
@@ -20,7 +20,7 @@ xhost +local:root
 #-e http://docs.docker.jp/v19.03/engine/reference/commandline/run.html#e-env-env-file
 #-it --name http://docs.docker.jp/v19.03/engine/reference/commandline/run.html#tty-name-it 
 docker run --ipc=host \
-  --device /dev/gpiomem \ #raspi gpio
+  --device /dev/gpiomem \
   --device /dev/i2c-1 \
   --device /dev/ttyACM0 \
   --privileged \
